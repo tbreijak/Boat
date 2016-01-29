f3e1d959a36a5b04b5a8a45743659317c7ecd55e
tbreijak <ab6131@wayne.edu>
2016-01-29 14:40:07 -0500

    add repo for boat
    add repo

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+ fill(100);
+ rect(0, 200, 800, 600);
+ 
+ fill(255);
+ arc(250, 240, 280, 280, 0, PI, TWO_PI, CHORD);
+ 
+ rect(230, 50, 10, 200);
+ triangle(240, 50, 300, 150, 240, 250);
+}
